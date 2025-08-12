fb89f941059c0f948f6f1ecfe9abe0849a9fd189
icanplaystar <gengjz2028@gmail.com>
2025-08-12 20:20:14 +1000

    save change

--- week2/src/App.vue
+++ week2/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import JSON from './components/JSON.vue';
+import JSON from './components/JSON.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
+  <Form />
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
