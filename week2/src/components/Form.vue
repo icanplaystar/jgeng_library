2634d45290e6581056d06168bff362e902a2c838
icanplaystar <gengjz2028@gmail.com>
2025-08-12 19:56:46 +1000

    form

---
+++ week2/src/components/Form.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <h1>User Information Form / Credentials</h1>
+    </div>
+</template>
+
+<script setup>
+// Our logic will go here
+</script>
+
+<style scoped>
+// Our logic will go here
+</style>
+
+
+import { ref } from 'vue';
+
+const formData = ref({
+    username: '',
+    password: '',
+    isAustralian: false,
+    reason: '',
+    gender: ''
+});
+
+const submittedCards = ref([]);
+
+const submitForm = () => {
+    submittedCards.value.push({
+        ...formData.value
+    });
+};
+
+<div class="row mt-5" v-if="submittedCards.length">
+   <div class="d-flex flex-wrap justify-content-start">
+      <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
+         <div class="card-header">
+            User Information
+         </div>
+         <ul class="list-group list-group-flush">
+            <li class="list-group-item">Username: {{ card.username }}</li>
+            <li class="list-group-item">Password: {{ card.password }}</li>
+            <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
+            <li class="list-group-item">Gender: {{ card.gender }}</li>
+            <li class="list-group-item">Reason: {{ card.reason }}</li>
+         </ul>
+      </div>
+   </div>
+</div>
+
+
+<style scoped>
+   .card {
+   border: 1px solid #ccc;
+   border-radius: 10px;
+   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+   }
+   .card-header {
+   background-color: #275FDA;
+   color: white;
+   padding: 10px;
+   border-radius: 10px 10px 0 0;
+   }
+   .list-group-item {
+   padding: 10px;
+   }
+</style>
